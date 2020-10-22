e2d968c5734afa294d6120aa3a7042f8306af3b8
Mathilde Arconte <signup@marconte.fr>
2020-10-22 17:01:36 +0200

    splash splash

--- webapp/src/views/Splashscreen/index.js
+++ webapp/src/views/Splashscreen/index.js
@@ -2,15 +2,24 @@ import './style.scss';
 
 import React,{ Component } from 'react'
 
-import { Button } from 'antd';
+import img from '../../assets/img/icon_mains.png'
+import logo from '../../assets/img/Logo.png'
 
 export default class Splashscreen extends Component {
 	render() {
 		return (
-			<div>
-				Splash
-				<Button type="primary">TOTO</Button>
-			</div>
+			<a href="/Accueil">
+				<section className="top">
+					<img src={logo} alt="borne to be alive logo" />
+					<img src={img} alt="icon mains" className="icon" />
+					<p>Désinfection des mains obligatoire avant l’utilisation de la borne.</p>
+				</section>
+				<section className="bottom">
+					<p>Insérez votre carte vitalepour vous identifier ou cliquez ici</p>
+					<div className="button"></div>
+				</section>
+
+			</a>
 		)
 	}
 }
