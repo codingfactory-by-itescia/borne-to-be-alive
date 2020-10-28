239ebe6958f7657fc48ebe8a8f8600c47abe1567
Mathilde Arconte <signup@marconte.fr>
2020-10-28 16:25:04 +0100

    crud-ticket api ticket functions

---
+++ webapp/src/services/api.js
@@ -0,0 +1,57 @@
+class Api {
+	static base_url = "http://localhost:5000/borne-to-be-alive/us-central1/api";
+	static async createTicket(ticket) {
+		const endpoint = "/ticket";
+
+		const conf = {
+			method: 'POST',
+			mode: 'cors',
+			cache: 'default',
+			body: JSON.stringify(ticket)
+		}
+		try {
+			const response = await fetch(Api.base_url + endpoint,conf);
+			const data = await response.json();
+
+			return data;
+		} catch (err) {
+			console.log(err);
+		}
+	}
+
+	static async getTickets() {
+		const endpoint = "/ticket";
+
+		const conf = {
+			method: 'GET',
+			mode: 'cors',
+			cache: 'default',
+		}
+		try {
+			const response = await fetch(Api.base_url + endpoint,conf);
+			const data = await response.json();
+
+			return data;
+		} catch (err) {
+			console.log(err);
+		}
+	}
+	static async updateTicketStatus(status) {
+		const endpoint = "/ticket";
+
+		const conf = {
+			method: 'PUT',
+			mode: 'cors',
+			cache: 'default',
+			body: JSON.stringify(status)
+		}
+		try {
+			const response = await fetch(Api.base_url + endpoint,conf);
+			const data = await response.json();
+
+			return data;
+		} catch (err) {
+			console.log(err);
+		}
+	}
+}
