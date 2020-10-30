0a3645a1b8f73e4fd38e23ac2bb2f55fe7b28142
Mathilde Arconte <signup@marconte.fr>
2020-10-30 16:40:54 +0100

    manageConsult update ticket (wip)

--- desktopapp/app/src/views/Medecin/index.js
+++ desktopapp/app/src/views/Medecin/index.js
@@ -1,41 +1,60 @@
 import './style.scss';
 
 import { Button, Layout } from 'antd';
-import React, { Component } from 'react'
+import React,{ Component } from 'react'
 
 import { Api } from '../../services/api.js'
 
-const { Content, Sider } = Layout;
+const { Content,Sider } = Layout;
 
 export default class Medecin extends Component {
-	state= {
-		tickets : [],
+	state = {
+		tickets: [],
+		currentConsult: {},
+		selectedConsult: {}
 	}
 	componentDidMount() {
 		this.getTickets()
-
+		this.getCurrentConsult()
 	}
 
 	getTickets = async () => {
-		this.setState({ tickets: await Api.getTickets()})
+		this.setState({ tickets: await Api.getTickets() })
 	}
+
+	getCurrentConsult = async () => {
+		const array = await Api.getTickets()
+		const currentConsult = array.filter(ticket => ticket.status === "in progress")
+		this.setState({ currentConsult: currentConsult[0]})
+	}
+
+	selectedConsult = (item) => {
+		this.setState({selectedConsult: item})
+	}
+
+	startConsult = async (item) => {
+		Api.updateTicketStatus("in progress")
+	}
+
 	render() {
+		const { tickets,currentConsult, selectedConsult } = this.state;
 		return (
 			<Layout className="Medecin">
 				<Content>
 					<h1><b>Bonjour</b> Dr Maboule</h1>
-					{this.state.tickets.map((ticket, index) => (
-						<div className="patientItem" key={index}>
+					{tickets.map((ticket,index) => (
+						<div className={`patientItem ${ticket.status === "in progress" ? "inProgess" : ""}`} key={index} onClick={() =>this.selectedConsult(ticket)}>
 							<p className="title">n°{ticket.id} {ticket.first_name} {ticket.last_name}</p>
 							{ticket.vitalId && (<p className="number">{ticket.vitalId}</p>)}
 						</div>
 					))}
 				</Content>
 				<Sider>
-					<h1>N°3</h1>
-					<h2>Rachel Bonneaux</h2>
-					<p>2 90 12 21 274 321 74</p>
-						<Button style={{width: '100%'}} href="#">Demarrer</Button>
+					<h1>N°{selectedConsult.id}</h1>
+					<h2>{selectedConsult.first_name} {selectedConsult.last_name}</h2>
+					<p>{selectedConsult.vitalId}</p>
+					{selectedConsult.status === "open" && (<Button style={{ width: '100%' }} onClick={this.startConsult}>Demarrer</Button>)}
+					{selectedConsult.status === "in progress" && (<Button style={{ width: '100%' }}>Terminer</Button>)}
 				</Sider>
 			</Layout>
 		)
